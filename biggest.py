387bc342f7592b43fdae97c607d755a751274ef3
Raji <166475387+Rajalekshmi1972@users.noreply.github.com>
2025-10-14 13:55:21 +0530

    Add files via upload

--- biggest.py
+++ biggest.py
@@ -1,13 +1,20 @@
-print("enter three numbers:")
-n1=int(input())
-n2=int(input())
-n3=int(input())
-if(n1>n2 and n1>n3)
-print("n1 is the largest")
-elseif(n2>n3 and n2>n1)
-print("n2 is the largest")
-else(n3>n1 and n3>n2)
-print("n3 is the largest")
+print("enter  the  three number:")
+a1=int(input())
+b1=int(input())
+c1=int(input())
+if(a1>b1 and a1>c1):
+    print(a1,"is greater")
+elif(b1>a1 and b1>c1):
+    print(b1,"is greater")
+else:
+    print(c1,"is greater")
+    
 
-
-      
+# Additional feature: Find the smallest of the three numbers
+print("\nFinding the smallest number:")
+if a1 < b1 and a1 < c1:
+    print(a1, "is the smallest")
+elif b1 < a1 and b1 < c1:
+    print(b1, "is the smallest")
+else:
+    print(c1, "is the smallest")
